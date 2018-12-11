e5277492c20344d6aa4c74abfc50d349e8e9dd17
dndubins <d.dubins@utoronto.ca>
2018-12-10 22:47:14 -0500

    Create RWstruct.ino

---
+++ examples/RWstruct/RWstruct.ino
@@ -0,0 +1,94 @@
+/* Example program for use with SRAMsimple.h - sending structures to 23LC1024 (**not tested yet**)
+    Arduino Uno Memory Expansion Sample Program
+    Author: This headache belonged to David Dubins 12-Dec-18
+    Library created and expanded by: D. Dubins 12-Nov-18
+
+   Sample program to use a Serial SRAM chip to expand memory for an Arduino Uno
+   giving access to an additional 128kB of random access memory.  The 23LC1024 uses
+   the Serial Peripheral Interface (SPI) to transfer data and commands between the
+   UNO and the memory chip.
+
+   Used the following components:
+   (1) Arduino Uno
+   (2) Microchip 23LC1024 SPI SRAM chip soldered on an Arduino Protoshield
+   
+   Wiring:
+   23LC1024 - Uno:
+   ---------------
+   Pin1 (JSC) -- Pin 10 (CS) (with 10K pullup to +5V)
+   Pin2 (SO)  -- Pin 12 (MISO)
+   Pin3 (NU)  -- 10K -- +5V
+   Pin4 (GND) -- GND
+   Pin5 (SI)  -- Pin 11 (MOSI)
+   Pin6 (SCK) -- Pin 13 (SCK)
+   Pin7 (HOLD) -- 10K -- +5V
+   Pin8 (V+)  -- +5V
+*/
+
+#include <SRAMsimple.h>
+
+#define CSPIN 10       // Default Chip Select Line for Uno (change as needed)
+
+SRAMsimple sram;       // initialize an instance of this class
+
+void setup()
+{
+  Serial.begin(9600);                         // set communication speed for the serial monitor
+  SPI.begin();                                // start communicating with the memory chip
+  
+  /*******************  Create a dummy structure and objects  ******************/
+  struct chemElements {  // declaring a structured array
+    char atomName[10];
+    int atomNum;
+    float MW;
+  } organicA[4],organicB[4]; //declare organicA to send data and organicB to receive data
+
+  union txUnion{
+    chemElements organicA[4];
+	  byte byteArrA[sizeof(organicA)]; // byte array sharing the same space
+  };
+
+  union txUnion txData; // create a new union instance called data2go
+  
+  // Fill objects with data
+  txData.organicA[0].atomName[10]="Hydrogen";
+  txData.organicA[0].atomNum=1;
+  txData.organicA[0].MW=1.000794;
+
+  txData.organicA[1].atomName[10]="Carbon";
+  txData.organicA[1].atomNum=6;
+  txData.organicA[1].MW=12.0107;
+
+  txData.organicA[2].atomName[10]="Nitrogen";
+  txData.organicA[2].atomNum=7;
+  txData.organicA[2].MW=14.0067;
+
+  txData.organicA[3].atomName[10]="Oxygen";
+  txData.organicA[3].atomNum=8;
+  txData.organicA[3].MW=15.9994;
+  
+/************  Write a Sequence of Bytes from an Array *******************/
+  Serial.println("\nWriting byte array using Sequential: ");
+  sram.WriteByteArray(0, txData.byteArrA, sizeof(txData.byteArrA));        // Write array to memory starting at address 0
+
+/************ Read a Sequence of Bytes from Memory into an Array **********/
+  union rxUnion{
+    chemElements organicB[4];
+	  byte byteArrB[sizeof(organicB)]; // byte array sharing the same space
+  };
+
+  union rxUnion rxData; // create a new union instance called data2go
+   
+  Serial.println("Reading byte array using sequential: ");
+  sram.ReadByteArray(0, rxData.byteArrB, sizeof(rxData.byteArrB));   // Read array into read_data_a starting at address 0
+
+  for(int i=0;i<4;i++){
+	  Serial.println(rxData.organicB[i].atomName); // print received data
+    Serial.println(rxData.organicB[i].atomNum); // print received data
+    Serial.println(rxData.organicB[i].MW); // print received data
+  }
+}
+
+void loop()
+{
+}
